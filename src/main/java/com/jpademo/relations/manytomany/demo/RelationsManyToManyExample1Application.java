99145c6bf2ae2d06e8a290c9eb81e776987005ce
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-17 14:56:25 +0530

    initial commit

---
+++ src/main/java/com/jpademo/relations/manytomany/demo/RelationsManyToManyExample1Application.java
@@ -0,0 +1,13 @@
+package com.jpademo.relations.manytomany.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsManyToManyExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsManyToManyExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpademo/relations/manytomany/demo/RelationsManyToManyExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpademo.relations.manytomany.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsManyToManyExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
